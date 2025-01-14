b14bebefa16f0205ea3da85ab86dc6ada99c118a
MrOrmo7372 <hakilong1@gmail.com>
2025-01-14 16:53:00 +0700

    Update Donkihote.lua

--- Donkihote.lua
+++ Donkihote.lua
@@ -2,46 +2,40 @@ local Vote_Start = game:GetService("ReplicatedStorage"):WaitForChild("endpoints"
 
 local VoteTime = 0
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Tạo giao diện chính
-local Window = OrionLib:MakeWindow({
-    Name = "MrHub", 
-    HidePremium = false, 
-    SaveConfig = true, -- Bật lưu config
-    ConfigFolder = "MrHubConfig" -- Tên thư mục lưu config
+local Window = Rayfield:CreateWindow({
+   Name = "MrHub AA",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Waiting For AA Script Load",
+   LoadingSubtitle = "by MrHub",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "MrHub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
--- Tạo tab
-local Tab = Window:MakeTab({
-    Name = "Farm",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false,
-})
-
--- Thêm Toggle có lưu cấu hình
-Tab:AddToggle({
-    Name = "Auto Start", -- Tên toggle
-    Default = false, -- Giá trị mặc định nếu không có file config
-    Save = true, -- Lưu giá trị vào config
-    Flag = "AutoStartGame_Toggle", -- Tên flag để lưu
-    Callback = function(Value)
-        print("Auto Start:", Value) -- In ra giá trị hiện tại
-        if Value and VoteTime < 1 then
-            VoteTime += 1
-            -- Gọi hàm từ server
-            local result = Vote_Start:InvokeServer()
-        end
-    end
-})
-
--- Hiển thị GUI
-OrionLib:Init()
-
--- Lấy giá trị sau khi giao diện được khởi tạo
-local AutoStartGameCF = OrionLib.Flags["AutoStartGame_Toggle"].Value
-print("AutoStartGameCF:", AutoStartGameCF)
-
-if AutoStartGameCF then
-    print("Auto Start đã bật từ file config!")
-end
+Rayfield:LoadConfiguration()
