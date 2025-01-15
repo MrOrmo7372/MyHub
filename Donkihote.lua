49d284bf9bd33fdaf1c3c3a82fd4c519df11ddf1
MrOrmo7372 <hakilong1@gmail.com>
2025-01-15 12:17:24 +0700

    Update Donkihote.lua

--- Donkihote.lua
+++ Donkihote.lua
@@ -1,4 +1,5 @@
 print("THIS IS THE START OF THE SCRIPT")
+wait(5)
 
 local Vote_Start = game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("vote_start")
 local Set_Game_Finish_Vote = game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("set_game_finished_vote")
