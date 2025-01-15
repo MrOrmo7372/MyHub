7bbf4842b7432580d166b6269b5016a845f2a3ba
MrOrmo7372 <hakilong1@gmail.com>
2025-01-15 13:28:59 +0700

    Update Donkihote.lua

--- Donkihote.lua
+++ Donkihote.lua
@@ -119,9 +119,9 @@ local ToggleFarmGems = FarmGems:CreateToggle({
    -- The variable (Value) is a boolean on whether the toggle is true or false
          if Value == true and game.PlaceId ~= 8304191830 then
             local Sakura_Table = {
-               [1] = {"{27b582bb-e814-4847-a519-ed1d9062c748}", nil},
-               [2] = {"{27b582bb-e814-4847-a519-ed1d9062c748}", nil},
-               [3] = {"{27b582bb-e814-4847-a519-ed1d9062c748}", nil}
+               [1] = {"{27b582bb-e814-4847-a519-ed1d9062c748}", CFrame.new(-2967.56396, 33.7417984, -715.313049, 1, 0, 0, 0, 1, 0, 0, 0, 1)},
+               [2] = {"{27b582bb-e814-4847-a519-ed1d9062c748}", CFrame.new(-2967.56396, 33.7417984, -710.313049, 1, 0, 0, 0, 1, 0, 0, 0, 1)},
+               [3] = {"{27b582bb-e814-4847-a519-ed1d9062c748}", CFrame.new(-2967.56396, 33.7417984, -705.313049, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
             }
             for i=1, 3 do
                while tonumber(MoneyPlayerText.Text) < 600 do
