5bbc016a1859680da0334824c4cc1be770f9762a
MrOrmo7372 <hakilong1@gmail.com>
2025-01-22 08:33:57 +0700

    Update Donkihote.lua

--- Donkihote.lua
+++ Donkihote.lua
@@ -212,7 +212,7 @@ setreadonly(mt, true)
 
 CheckPOPUP.Changed:Connect(function()
    if CheckPOPUP.Value == true then
-      MoneyChange_POPUP_UI:ChildAdded:Wait()
+      MoneyChange_POPUP_UI.ChildAdded:Wait()
       local Money = CheckMoney_POPUP_GUI()
       MARCO_TABLE[STEP - 1].Cost_Money = Money
       print(MARCO_TABLE[STEP - 1].Event_Type)
@@ -225,7 +225,7 @@ end)
 
 CheckPOPUP_Upgrade.Changed:Connect(function()
    if CheckPOPUP_Upgrade.Value == true then
-      MoneyChange_POPUP_UI:ChildAdded:Wait()
+      MoneyChange_POPUP_UI.ChildAdded:Wait()
       local Money = CheckMoney_POPUP_GUI()
       MARCO_TABLE[STEP - 1].Cost_Money = Money
       print(MARCO_TABLE[STEP - 1].Event_Type)
