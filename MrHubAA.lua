1ca080bc01c6abf69e5f9fb524dc6f8f6b49aa56
MrOrmo7372 <hakilong1@gmail.com>
2025-02-10 16:58:26 +0700

    Update MrHubAA.lua

--- MrHubAA.lua
+++ MrHubAA.lua
@@ -182,6 +182,7 @@ mt.__namecall = function(self, ...)
                     table.insert(MARCO_TABLE, unitData)
                     print("Đã ghi lại đợt đặt Unit:", unitData.Unit_Type)
                     print(unitData.CFrame.Position[1])
+                    saveMacroData()
                 end
             else -- FireServer
                 local unitData = {
